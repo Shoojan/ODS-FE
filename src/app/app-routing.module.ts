9134dc61ba50d94da07510a7e9818f9f2916a362
Sujan Maharjan <sujan.mhrzn2@gmail.com>
2021-05-02 10:49:12 +0545

    Add to cart dynamic field update made, routing corrected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
 import { OrdersComponent } from './components/orders/orders.component';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'orders' },
   { path: 'orders', component: OrdersComponent },
-  { path: 'checkout', component: CheckoutComponent }
+  { path: 'checkout', component: CheckoutComponent },
+  { path: '**', redirectTo: 'orders' },
 ];
 
 @NgModule({
--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -11,23 +11,53 @@ import { OrdersService } from 'src/app/services/orders.service';
 export class OrdersComponent implements OnInit {
 
   public orders: Order[] = [];
+  public tempOrders: Order[] = [];
   public totalCost: number = 0;
+  public tempTotalCost: number = 0;
 
   constructor(private orderService: OrdersService) { }
 
   ngOnInit(): void {
     this.getOrders(1);
-
   }
 
   public getOrders(customerId: number): void {
     this.orderService.getOrders(customerId).subscribe(
       (res: Order[]) => {
         this.orders = res;
+        this.tempOrders = this.orders;
+        this.totalCost = this.orders.reduce((acc, cv) => acc + cv.totalPrice, 0);
+        this.tempTotalCost = this.totalCost;
       },
       (err: HttpErrorResponse) => {
         alert(err.message);
       }
     )
   }
+
+  public updatePrice(event: any, orderId: number) {
+    this.tempOrders.forEach(order => {
+      if (order.id == orderId) {
+        order.quantity = event.target.value;
+        const previousTotalCost = order.totalPrice;
+        order.totalPrice = order.quantity * order.unitPrice;
+        this.tempTotalCost = this.tempTotalCost - previousTotalCost + order.totalPrice;
+      }
+    })
+  }
+
+  public removeOrder(orderId: number) {
+    if (confirm("Are you sure you want to remove?")) {
+      const order = this.tempOrders.find(x => x.id == orderId);
+      if (order) {
+        const index = this.tempOrders.indexOf(order);
+        if (index > -1) {
+          this.tempOrders.splice(index, 1);
+          this.tempTotalCost = this.tempTotalCost - order.totalPrice;
+        }
+      }
+    }
+  }
+
+
 }
